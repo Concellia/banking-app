de73f0d349341c33285ccb14b1aebb9220ca4439
VF-ROOT\rhant001 <tiyiselaniconcellia.rhangani@vcontractor.co.za>
2023-10-21 00:39:42 +0200

    button template component and unit tests

---
+++ src/components/button-template.vue
@@ -0,0 +1,26 @@
+<template>
+  <article>
+    <section class="button-template">
+      <button class="button" @click="buttonClick">{{ buttonText }}</button>
+    </section>
+  </article>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+defineProps<{
+  buttonText: string
+}>()
+
+const emit = defineEmits<{
+  (e: 'on-button-click'): void
+}>()
+
+const buttonClick = () => {
+  emit('on-button-click')
+}
+</script>
+
+<style scoped lang="less">
+@import '../styles/main/components/button-templete.less';
+</style>
